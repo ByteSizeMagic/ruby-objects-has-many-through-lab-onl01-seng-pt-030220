f5070adfc3727555a65b3bcc9232543eab99197e
Crystal Schooling <bytesizemagic@gmail.com>
2020-04-02 09:48:33 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,29 @@
+class Patient
+
+  @@all = []
+
+  attr_accessor :name
+
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointments.all.select do |appt|
+      appt.patient == self
+    end
+  end
+
+  def doctors
+    appointments.map do |appt|
+      appt.doctor
+    end
+  end
+
+end
